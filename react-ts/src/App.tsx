6d874d9a865487ef18253e52888098da073a5ca5
Bohdan Mykytey <bohdanmykytey@Bohdans-MacBook-Pro.local>
2022-04-08 17:43:02 -0400

    added first peace of state which is todo and setTodo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,16 @@
+
+import React, {useState} from 'react';
 import "./App.css";
 import InputField from "./components/InputField"
 
 //declaring react functional component
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo}/>
 
     </div>
   )
--- react-ts/src/components/InputField.tsx
+++ react-ts/src/components/InputField.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-const InputField = () => {
+interface todoProps  {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({todo, setTodo}: todoProps) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input_box" />
+      <input 
+        type="input" 
+        placeholder="Enter a task" 
+        className="input_box" 
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
+        />
 
       <button className="input_submit" type="submit">
         GO
