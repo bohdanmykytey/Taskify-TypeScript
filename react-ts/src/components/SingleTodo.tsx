d93ae045a34473e3f7d86db0ca04dfc515a01865
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2022-04-11 18:20:10 -0400

    added cross out and delete functionality

--- react-ts/src/components/SingleTodo.tsx
+++ react-ts/src/components/SingleTodo.tsx
@@ -1,34 +1,49 @@
-import { Todo } from '../model'
-import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
-import { MdDone } from 'react-icons/md'
-import './styles.css'
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
 
 interface singleTodo {
-    todo: Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-function SingleTodo({todo, todos, setTodos} : singleTodo) {
+function SingleTodo({ todo, todos, setTodos }: singleTodo) {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
-      <form className="todos_single">
-          <span className="todos_single--text">{todo.todo}</span>
+    <form className="todos_single">
+      {todo.isDone ? (
+        <s className="todos_single--text">{todo.todo}</s> //s tag ads a strike through the text to mark done
+      ) : (
+        <span className="todos_single--text">{todo.todo}</span>
+      )}
+      <div>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
 
-          <div>
-              <span className="icon">
-                <AiFillDelete />
-              </span>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
 
-              <span className="icon">
-                <AiFillEdit />
-              </span>
-
-              <span className="icon">
-                <MdDone />
-              </span>
-          </div>
-      </form>
-  )
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 }
 
-export default SingleTodo
+export default SingleTodo;
